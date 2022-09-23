e09f5dd6971892d8e8d8741a826f1e15eaf7ad51
Phil <ftcpjw@yahoo.com>
2022-09-22 21:25:30 -0400

    finshed game state

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/pwinning1991/deck"
@@ -49,54 +50,105 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
+func Shuffle(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Deck = deck.New(deck.Deck(3), deck.Shuffle)
+	return ret
+}
+
+func Deal(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Player = make(Hand, 0, 5)
+	ret.Dealer = make(Hand, 0, 5)
+	var card deck.Card
+	for i := 0; i < 2; i++ {
+		card, ret.Deck = draw(ret.Deck)
+		ret.Player = append(ret.Player, card)
+		card, ret.Deck = draw(ret.Deck)
+		ret.Dealer = append(ret.Dealer, card)
+	}
+
+	ret.State = StatePlayerTurn
+	return ret
+}
+
+func Stand(gs GameState) GameState {
+	ret := clone(gs)
+	ret.State++
+	return ret
+
+}
+
+func Hit(gs GameState) GameState {
+	ret := clone(gs)
+	hand := ret.CurrentPlayer()
+	var card deck.Card
+	card, ret.Deck = draw(ret.Deck)
+	*hand = append(*hand, card)
+	if hand.Score() > 21 {
+		Stand(ret)
+	}
+	return ret
+}
+
+func EndHand(gs GameState) GameState {
+	ret := clone(gs)
+	pScore, dScore := ret.Player.Score(), ret.Dealer.Score()
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player:", ret.Player)
+	fmt.Println("Dealer:", ret.Dealer)
+	fmt.Println("Player Score:", pScore)
+	fmt.Println("Dealer Score:", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("You Busted")
+	case dScore > 21:
+		fmt.Println("Dealer Busted")
+	case pScore > dScore:
+		fmt.Println("You Win")
+	case pScore < dScore:
+		fmt.Println("You Lose")
+	case pScore == dScore:
+		fmt.Println("Draw")
+	}
+	fmt.Println()
+	ret.Player = nil
+	ret.Dealer = nil
+	return ret
+}
+
 func main() {
 	//cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var gs GameState
-	gs.Deck = deck.New(deck.Deck(3), deck.Shuffle)
-	//var card deck.Card
-	//var player, dealer Hand
-	//for i := 0; i < 2; i++ {
-	//for _, hand := range []*Hand{&player, &dealer} {
-	//card, cards = draw(cards)
-	//*hand = append(*hand, card)
-	//}
-	//}
-	//var input string
-	//for input != "s" {
-	//fmt.Println("Player:", player)
-	//fmt.Println("Player Score:", player.Score())
-	//fmt.Println("Dealer:", dealer.DealerString())
-	//fmt.Println("What will you do? (h)it, (s)tand")
-	//fmt.Scanf("%s\n", &input)
-	//switch input {
-	//case "h":
-	//card, cards = draw(cards)
-	//player = append(player, card)
-	//}
-	//}
-	//for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
-	//card, cards = draw(cards)
-	//dealer = append(dealer, card)
-	//}
-	//pScore, dScore := player.Score(), dealer.Score()
-	//fmt.Println("==FINAL HANDS==")
-	//fmt.Println("Player:", player)
-	//fmt.Println("Dealer:", dealer)
-	//fmt.Println("Player Score:", pScore)
-	//fmt.Println("Dealer Score:", dScore)
-	//switch {
-	//case pScore > 21:
-	//fmt.Println("You Busted")
-	//case dScore > 21:
-	//fmt.Println("Dealer Busted")
-	//case pScore > dScore:
-	//fmt.Println("You Win")
-	//case pScore < dScore:
-	//fmt.Println("You Lose")
-	//case pScore == dScore:
-	//fmt.Println("Draw")
+	gs = Shuffle(gs)
+	gs = Deal(gs)
+	var input string
+	for gs.State == StatePlayerTurn {
+		fmt.Println("Player:", gs.Player)
+		fmt.Println("Player Score:", gs.Player.Score())
+		fmt.Println("Dealer:", gs.Dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			gs = Hit(gs)
+		case "s":
+			gs = Stand(gs)
+		default:
+			fmt.Println("Invalid option: chose either \"h\" or \"s\"")
+		}
 
-	//}
+	}
+
+	for gs.State == StateDealerTurn {
+		if gs.Dealer.Score() <= 16 || (gs.Dealer.Score() == 17 && gs.Dealer.MinScore() != 17) {
+			gs = Hit(gs)
+		} else {
+			gs = Stand(gs)
+		}
+	}
+
+	gs = EndHand(gs)
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
